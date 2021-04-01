1c6df832251094565f7b16f662c34116376360b1
seungb5 <seungbo1112@gmail.com>
2021-04-01 12:37:03 +0900

    Add files

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seuan <seuan@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/01 11:31:20 by seuan             #+#    #+#             */
+/*   Updated: 2021/04/01 12:35:44 by seuan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+int					get_next_line(int fd, char **line);
+int					has_return(char *str);
+
+size_t				ft_strlen(const char *s);
+
+char				*join_str(const char *s1, const char *s2);
+
+#endif
